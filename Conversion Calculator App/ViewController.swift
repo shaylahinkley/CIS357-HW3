e289818aab56935236dc4519e34c3cfbd0286448
Shayla Hinkley <hinkleys@mail.gvsu.edu>
2020-09-25 06:36:52 -0400

    Working Part A

---
+++ Conversion Calculator App/ViewController.swift
@@ -0,0 +1,80 @@
+
+//  ViewController.swift
+//  Conversion Calculator App
+//
+//  Created by Shayla Hinkley on 9/25/20.
+//  Copyright © 2020 Shayla Hinkley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var lengthTopEnterValue: DecimalMinusTextField!
+    
+    
+    @IBOutlet weak var lengthBottomEnterValue: DecimalMinusTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.lengthTopEnterValue.delegate = self
+        self.lengthBottomEnterValue.delegate = self
+    }
+    
+    func calcYardToMeters(yards: String) -> String {
+        var numMeters: Double
+        numMeters = Double(yards)! * 0.9144
+        return String(numMeters)
+    }
+    
+    func calcMeterToYards(meters: String) -> String {
+        var numYards: Double
+        numYards = Double(meters)! * 1.0936133
+        return String (numYards)
+    }
+
+
+    @IBAction func calculateButtonPressed(_ sender: Any) {
+        if lengthTopEnterValue.text! != "" {
+            lengthBottomEnterValue.text! = calcYardToMeters(yards: lengthTopEnterValue.text!)
+            
+        }
+        if lengthBottomEnterValue.text! != "" {
+            lengthTopEnterValue.text! = calcMeterToYards(meters: lengthBottomEnterValue.text!)
+        }
+        self.view.endEditing(true)
+        
+    }
+    @IBAction func clearButtonPressed(_ sender: Any) {
+        lengthBottomEnterValue.text = nil
+        lengthTopEnterValue.text = nil
+        self.view.endEditing(true)
+    }
+    @IBAction func dismissKeyBoardTap(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+        
+       return true
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if(textField == self.lengthTopEnterValue) {
+            lengthBottomEnterValue.text = nil
+        } else {
+            lengthTopEnterValue.text = nil
+        }
+    }
+    
+    
+}
+
+
+
+
+
---
+++ Conversion Calculator AppTests/Conversion_Calculator_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Conversion_Calculator_AppTests.swift
+//  Conversion Calculator AppTests
+//
+//  Created by Shayla Hinkley on 9/25/20.
+//  Copyright © 2020 Shayla Hinkley. All rights reserved.
+//
+
+import XCTest
+@testable import Conversion_Calculator_App
+
+class Conversion_Calculator_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
